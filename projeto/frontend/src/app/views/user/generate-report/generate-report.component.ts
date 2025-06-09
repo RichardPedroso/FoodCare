f958cfc992f24a1ddc82a5e3836aea91a42f45ba
Eugenio <eugeniojosesilva2@gmail.com>
2025-06-09 00:18:26 -0300

    feat: criacao e adicao da base do component generate-report

---
+++ projeto/frontend/src/app/views/user/generate-report/generate-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-report',
+  imports: [],
+  templateUrl: './generate-report.component.html',
+  styleUrl: './generate-report.component.css'
+})
+export class GenerateReportComponent {
+
+}
