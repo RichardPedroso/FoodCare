585e190c16b06aae03677eeabafea113f16ba68c
Eugenio <eugeniojosesilva2@gmail.com>
2025-06-09 00:17:34 -0300

    feat: adicao da base do component main

--- projeto/frontend/src/app/views/app/main/main.component.ts
+++ projeto/frontend/src/app/views/app/main/main.component.ts
@@ -1,56 +1,11 @@
 import { Component } from '@angular/core';
-import { routes } from '../../../app.routes';
-import { Router, RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import * as fontawesome from '@fortawesome/free-solid-svg-icons'
-
 
 @Component({
-  selector: 'app-main',
-  imports: [
-
-    RouterOutlet,
-    MatToolbarModule,
-    FormsModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatIconModule,
-    MatListModule,
-    MatExpansionModule,
-    MatTooltipModule,
-    FontAwesomeModule,
-
-  ],
+  selector: 'app-home',
+  imports: [],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
-
-  faFootericon = fontawesome.faHeartBroken;
-
-
-  constructor(private router: Router){}
-
-
-
-  public logout() {
-    console.log('logout clicado');
-    this.router.navigate(['account/sign-in'])
-  }
-
-
-
-
+export class HomeComponent {
 
 }
