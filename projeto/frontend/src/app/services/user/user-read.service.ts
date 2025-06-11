ea59e843443f93f220a2fe3653853079f5f4f8db
Richard <ricfilho2004@gmail.com>
2025-06-11 20:56:59 -0300

    feat: adicao de services de user

--- projeto/frontend/src/app/services/user/user-read.service.ts
+++ projeto/frontend/src/app/services/user/user-read.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { firstValueFrom } from 'rxjs';
+import { User } from '../../domain/model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserReadService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAll(): Promise<any> {
+
+    // para consumir este metodo, basta utilizar a palavra chave: await
+    //exemplo:
+    //let variavel = await user.ReadService.findAll();
+
+    return firstValueFrom(this.http.get<any>(`${environment.api_endpoint}/user`));
+  }
+
+  findById(id: String): Promise<User>{
+    return firstValueFrom(this.http.get<any>(`${environment.api_endpoint}/user/${id}`));
+  }
+
+  findByEmail(email: String): Promise<User>{
+    return firstValueFrom(this.http.get<any>(`${environment.api_endpoint}/user/?email=${email}`));
+  }
+
 }
