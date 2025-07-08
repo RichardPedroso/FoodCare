a00c77c7e2c38774653c6d8e40e85af3465665d0
Eugenio <eugeniojosesilva2@gmail.com>
2025-07-07 22:56:12 -0300

    feat: adicao da logica para exibir o municipio em my-profile

--- projeto/frontend/src/app/views/account/my-profile/my-profile.component.ts
+++ projeto/frontend/src/app/views/account/my-profile/my-profile.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { AuthenticationService } from '../../../services/security/authentication.service';
+import { MunicipalityReadService } from '../../../services/municipality/municipality-read.service';
+import { Municipality } from '../../../domain/model/municipality';
 
 @Component({
   selector: 'app-my-profile',
@@ -17,10 +19,24 @@ import { AuthenticationService } from '../../../services/security/authentication
 })
 export class MyProfileComponent implements OnInit {
   user: any;
+  municipality: Municipality | null = null;
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private municipalityReadService: MunicipalityReadService
+  ) {}
 
   ngOnInit(): void {
     this.user = this.authenticationService.getCurrentUser();
+    if (this.user?.municipality_id) {
+      this.municipalityReadService.getById(this.user.municipality_id).subscribe({
+        next: (municipality) => {
+          this.municipality = municipality;
+        },
+        error: (error) => {
+          console.error('Erro ao buscar municipality:', error);
+        }
+      });
+    }
   }
 }
