bddcdc237bd4b01e1199977e28967c1e3438b0f1
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-19 20:23:16 -0300

    refactor: atualizacao do UpdateUserDto com todos os atributos necessarios

--- projeto/backend/src/main/java/br/com/faitec/foodcare/domain/dto/UpdateUserDto.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/domain/dto/UpdateUserDto.java
@@ -4,14 +4,26 @@ import br.com.faitec.foodcare.domain.UserModel;
 
 public class UpdateUserDto {
     private int id;
-    private String fullName;
+    private String name;
     private String email;
+    private String phone;
+    private UserModel.UserType userType;
+    private boolean isAdmin;
+    private double familyIncome;
+    private int peopleQuantity;
+    private int municipalityId;
 
     public UserModel toUserModel(){
         final UserModel entity = new UserModel();
         entity.setId(id);
-        entity.setFullname(fullName);
+        entity.setName(name);
         entity.setEmail(email);
+        entity.setPhone(phone);
+        entity.setUserType(userType);
+        entity.setAdmin(isAdmin);
+        entity.setFamilyIncome(familyIncome);
+        entity.setPeopleQuantity(peopleQuantity);
+        entity.setMunicipalityId(municipalityId);
         return entity;
     }
 
@@ -23,12 +35,12 @@ public class UpdateUserDto {
         this.id = id;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getName() {
+        return name;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -37,5 +49,53 @@ public class UpdateUserDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public UserModel.UserType getUserType() {
+        return userType;
+    }
+    
+    public void setUserType(UserModel.UserType userType) {
+        this.userType = userType;
+    }
+    
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+    
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+    
+    public double getFamilyIncome() {
+        return familyIncome;
+    }
+    
+    public void setFamilyIncome(double familyIncome) {
+        this.familyIncome = familyIncome;
+    }
+    
+    public int getPeopleQuantity() {
+        return peopleQuantity;
+    }
+    
+    public void setPeopleQuantity(int peopleQuantity) {
+        this.peopleQuantity = peopleQuantity;
+    }
+    
+    public int getMunicipalityId() {
+        return municipalityId;
+    }
+    
+    public void setMunicipalityId(int municipalityId) {
+        this.municipalityId = municipalityId;
     }
 }
