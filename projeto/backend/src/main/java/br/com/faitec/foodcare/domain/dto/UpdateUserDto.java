afee4273b2793a6b9692062e9452d0e8034f019a
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-19 21:26:44 -0300

    refactor: Atualização de UpdateUserDto em relação ao Lombok

--- projeto/backend/src/main/java/br/com/faitec/foodcare/domain/dto/UpdateUserDto.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/domain/dto/UpdateUserDto.java
@@ -1,7 +1,9 @@
 package br.com.faitec.foodcare.domain.dto;
 
 import br.com.faitec.foodcare.domain.UserModel;
+import lombok.Data;
 
+@Data
 public class UpdateUserDto {
     private int id;
     private String name;
@@ -27,75 +29,4 @@ public class UpdateUserDto {
         return entity;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getPhone() {
-        return phone;
-    }
-    
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    
-    public UserModel.UserType getUserType() {
-        return userType;
-    }
-    
-    public void setUserType(UserModel.UserType userType) {
-        this.userType = userType;
-    }
-    
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-    
-    public void setAdmin(boolean isAdmin) {
-        this.isAdmin = isAdmin;
-    }
-    
-    public double getFamilyIncome() {
-        return familyIncome;
-    }
-    
-    public void setFamilyIncome(double familyIncome) {
-        this.familyIncome = familyIncome;
-    }
-    
-    public int getPeopleQuantity() {
-        return peopleQuantity;
-    }
-    
-    public void setPeopleQuantity(int peopleQuantity) {
-        this.peopleQuantity = peopleQuantity;
-    }
-    
-    public int getMunicipalityId() {
-        return municipalityId;
-    }
-    
-    public void setMunicipalityId(int municipalityId) {
-        this.municipalityId = municipalityId;
-    }
 }
