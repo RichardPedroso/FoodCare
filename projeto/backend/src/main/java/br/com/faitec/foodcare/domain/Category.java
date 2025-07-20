406471018e7069cece6d5e2138157dc6af5c7e4e
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-20 00:13:41 -0300

    feat: adicao da classe Category

---
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/domain/Category.java
@@ -0,0 +1,14 @@
+package br.com.faitec.foodcare.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private int id;
+    private String description;
+    private boolean isActive;
+}
