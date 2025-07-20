e367677724e5e0db2b54e69be32eeee1da2fe2bd
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-19 21:13:11 -0300

    refactor: atualizacao do UpdateQuantityDao com todos os atributos necessarios

--- projeto/backend/src/main/java/br/com/faitec/foodcare/port/dao/product/UpdateQuantityDao.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/port/dao/product/UpdateQuantityDao.java
@@ -1,5 +1,5 @@
 package br.com.faitec.foodcare.port.dao.product;
 
 public interface UpdateQuantityDao {
-    boolean updateQuantity(final int id, final int newQuantity);
+    boolean updateQuantity(final int id, final int newStock);
 }
