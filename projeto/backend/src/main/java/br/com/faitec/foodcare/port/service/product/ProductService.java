dc8aead6cffdbb5e3e1809811d59e24af89f9b27
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-19 22:34:05 -0300

    refactor: retirada do atributo expirationDate de ProductService

--- projeto/backend/src/main/java/br/com/faitec/foodcare/port/service/product/ProductService.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/port/service/product/ProductService.java
@@ -3,5 +3,5 @@ package br.com.faitec.foodcare.port.service.product;
 import br.com.faitec.foodcare.domain.Product;
 import br.com.faitec.foodcare.port.service.crud.CrudService;
 
-public interface ProductService extends CrudService<Product>, UpdateNameService, UpdateQuantityService, UpdateExpirationDateService {
+public interface ProductService extends CrudService<Product>, UpdateNameService, UpdateQuantityService {
 }
