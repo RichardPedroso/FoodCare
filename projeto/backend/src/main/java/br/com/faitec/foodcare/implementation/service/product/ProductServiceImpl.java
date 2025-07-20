0254b56ae11379198c0801523358b1b14e961bda
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-19 21:11:44 -0300

    refactor: atualizacao do ProductServiceImpl com todos os atributos necessarios

--- projeto/backend/src/main/java/br/com/faitec/foodcare/implementation/service/product/ProductServiceImpl.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/implementation/service/product/ProductServiceImpl.java
@@ -82,14 +82,14 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public boolean updateQuantity(int id, int newQuantity) {
+    public boolean updateQuantity(int id, int newStock) {
         Product product = findById(id);
 
         if(product == null){
             return false;
         }
 
-        return productDao.updateQuantity(id, newQuantity);
+        return productDao.updateQuantity(id, newStock);
     }
 
     @Override
