562b178ab20be4db3af50dc3478c5b4d610de7db
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-07-19 21:26:28 -0300

    refactor: Atualização de UpdateProductDto em relação ao Lombok

--- projeto/backend/src/main/java/br/com/faitec/foodcare/domain/dto/UpdateProductDto.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/domain/dto/UpdateProductDto.java
@@ -1,7 +1,9 @@
 package br.com.faitec.foodcare.domain.dto;
 
 import br.com.faitec.foodcare.domain.Product;
+import lombok.Data;
 
+@Data
 public class UpdateProductDto {
     private int id;
     private String name;
@@ -26,67 +28,5 @@ public class UpdateProductDto {
         return entity;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public int getBasketQuantity() {
-        return basketQuantity;
-    }
-
-    public void setBasketQuantity(int basketQuantity) {
-        this.basketQuantity = basketQuantity;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(String expirationDate) {
-        this.expirationDate = expirationDate;
-    }
 }
