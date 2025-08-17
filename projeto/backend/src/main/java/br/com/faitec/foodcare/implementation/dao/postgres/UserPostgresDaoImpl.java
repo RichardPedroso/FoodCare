d8a14a3476382de917b72cb6035a1ef53748d73b
Tiago Cerqueira <tiago12cerqueira@gmail.com>
2025-08-16 22:33:05 -0300

    feat: Adição da classe UserPostgresDaoImpl

---
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/implementation/dao/postgres/UserPostgresDaoImpl.java
@@ -0,0 +1,86 @@
+package br.com.faitec.foodcare.implementation.dao.postgres;
+
+import br.com.faitec.foodcare.domain.UserModel;
+import br.com.faitec.foodcare.port.dao.user.UserDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserPostgresDaoImpl implements UserDao {
+
+    private final Connection connection;
+
+    public UserPostgresDaoImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public int create(UserModel entity) {
+        String sql = "INSERT INTO user_model(password, fullname, email, role) VALUES (?, ?, ?, ?)";
+
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, entity.getPassword());
+            preparedStatement.setString(2, entity.getFullname());
+            preparedStatement.setString(3, entity.getEmail());
+            preparedStatement.setString(4, entity.getRole().name());
+
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+            int id = 0;
+            if(resultSet.next()){
+                id = resultSet.getInt(1);
+            }
+
+            connection.commit();
+            resultSet.close();
+            preparedStatement.close();
+
+            return id;
+
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public UserModel findByid(int id) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void update(int id, UserModel entity) {
+
+    }
+
+    @Override
+    public UserModel findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public boolean updatePassword(int id, String newPassword) {
+        return false;
+    }
+}
