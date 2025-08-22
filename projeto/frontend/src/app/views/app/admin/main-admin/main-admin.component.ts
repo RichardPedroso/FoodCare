e2ba97628aab8980b8b37e39680e3679878a4bb6
Eugênio <eugeniojosesilva2@gmail.com>
2025-08-21 21:42:21 -0300

    feat: criacao do main-admin component

---
+++ projeto/frontend/src/app/views/app/admin/main-admin/main-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-admin',
+  imports: [],
+  templateUrl: './main-admin.component.html',
+  styleUrl: './main-admin.component.css'
+})
+export class MainAdminComponent {
+
+}
