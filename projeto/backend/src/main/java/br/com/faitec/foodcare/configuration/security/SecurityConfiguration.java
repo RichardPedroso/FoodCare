aa9438f78827c4d4415bc4384bbd4f7cb863c2da
Richard <ricfilho2004@gmail.com>
2025-09-09 22:46:18 -0300

    feat: adicao de novos metodos em security

--- projeto/backend/src/main/java/br/com/faitec/foodcare/configuration/security/SecurityConfiguration.java
+++ projeto/backend/src/main/java/br/com/faitec/foodcare/configuration/security/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package br.com.faitec.foodcare.configuration.security;
 
+import br.com.faitec.foodcare.domain.UserModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -37,12 +39,31 @@ public class SecurityConfiguration {
 
         http
                 .cors(
-                        cors -> cors.configurationSource(corsConfigurationSource()))
+                        cors -> cors.configurationSource(corsConfigurationSource())
+                )
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/authenticate")
-                        .permitAll())
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers(
+                                                "/swagger-ui/**",
+                                                "swagger-ui.html",
+                                                "/v3/api-docs/**",
+                                                "/authenticate",
+                                                "/api/user/**",
+                                                "/health",
+                                                "/",
+                                                "/docs"
+                                        ).permitAll()
+                                        .requestMatchers("/api/product/**"
+                                        ).hasAuthority(UserModel.UserType.ADMIN.name())
+                                        .anyRequest().authenticated()
+                )
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(
+                        headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+        ;
 
 
         return http.build();
